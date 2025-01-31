7185285dac1fccc1fe49af76b2dd14d1d3c4dfb0
Brian <aqw86645sd@gmail.com>
2025-01-31 23:46:52 +0800

    update news-api usage

--- utils/news.py
+++ utils/news.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from newsapi import NewsApiClient
@@ -11,12 +11,12 @@ class NewsAPI:
         self.api_token = class_config.NEWS_TOKEN
 
     def get_news(self, ticker):
-        date = self.get_new_york_date()
+        date = self.get_new_york_yesterday()
 
         newsapi = NewsApiClient(api_key=self.api_token)
 
         articles = newsapi.get_everything(
-            q='AAPL',
+            q=ticker,
             from_param=date,
             sort_by='publishedAt'
         )
@@ -25,11 +25,18 @@ class NewsAPI:
 
 
     @staticmethod
-    def get_new_york_date():
+    def get_new_york_today():
         new_york_timezone = pytz.timezone("America/New_York")
         new_york_time = datetime.now(new_york_timezone)
         new_york_date_str = new_york_time.strftime("%Y-%m-%d")
         return new_york_date_str
+
+    @staticmethod
+    def get_new_york_yesterday():
+        new_york_timezone = pytz.timezone("America/New_York")
+        new_york_time = datetime.now(new_york_timezone) - timedelta(days=1)
+        new_york_yesterday_str = new_york_time.strftime("%Y-%m-%d")
+        return new_york_yesterday_str
 
 
 if __name__ == "__main__":
